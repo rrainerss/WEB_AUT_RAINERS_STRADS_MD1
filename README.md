6273f7e6001bf036fc013faac14b6624fcee1101
rrainerss <rainersstrads8@gmail.com>
2024-06-02 22:32:38 +0300

    Naming and comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WEB_AUT_RAINERS_STRADS_MD1
  
+This is the first testing homework
+IT22033
--- cypress/e2e/demoqa/grid_selection.cy.js
+++ cypress/e2e/demoqa/grid_selection.cy.js
@@ -1,13 +1,19 @@
-describe("Demo QA", () => {
+//Rainers Strads IT2
+
+describe("Grid Selection", () => {
 
     it("form", () => {
+        //Visit site
         cy.visit("https://demoqa.com/selectable");
+
+        //Select grid elements
         cy.get('#demo-tab-grid').click();
         cy.get('#row1 > :nth-child(2)').click();
         cy.get('#row2 > :nth-child(1)').click();
         cy.get('#row2 > :nth-child(3)').click();
         cy.get('#row3 > :nth-child(2)').click();
 
+        //Confirm correct selection
         cy.get('#row1 > :nth-child(1)').contains('One').should('not.have.class', 'active');
         cy.get('#row1 > :nth-child(3)').contains('Three').should('not.have.class', 'active');
         cy.get('#row2 > :nth-child(2)').contains('Five').should('not.have.class', 'active');
